79f9db703c1647a25747bf803d1d782aa5ce9ed1
iqbalmtgjr <iqbalmtgjr@gmail.com.com>
2021-03-05 20:44:39 +0700

    nambah kelola user

---
+++ resources/views/kelola_user/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h1>Kelola User</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
