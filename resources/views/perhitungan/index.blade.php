ed6326dd265d33493b9884212f580084c002b467
iqbalmtgjr@gmail.com <iqbalmtgjr@gmail.com>
2021-08-25 20:09:38 +0700

    web

--- resources/views/perhitungan/index.blade.php
+++ resources/views/perhitungan/index.blade.php
@@ -1,30 +1,30 @@
 @extends('layouts.master')
 @section('aktif_perhitungan', 'active')
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3>Perhitungan</h3>
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3>Perhitungan</h3>
+            </div>
+            <div class="panel-body">
+                <div class="row" style="margin-left: 10px; margin-top: 10px">
+                    <button class="btn btn-primary btn-md" data-toggle="modal" data-target="#tambah"><i
+                            class="fa fa-user-plus"></i> Tambah</button> <br>
                 </div>
-                <div class="panel-body">
-                    <div class="row" style="margin-left: 10px; margin-top: 10px">
-                        <button class="btn btn-primary btn-md" data-toggle="modal" data-target="#tambah"><i
-                                class="fa fa-user-plus"></i> Tambah</button> <br>
-                    </div>
-                    {{-- @include('penggunaan_lahan/modaltambah') --}}
-                    <div class="row" style="margin-top: 20px; margin-left: 10px; margin-right: 10px">
-                        <table class="table" id="datatable">
-                            <thead>
-                                <tr>
-                                    <th scope="col">No</th>
-                                    <th scope="col">Nama Kecamatan</th>
-                                    <th scope="col">Luas Penggunaan Lahan</th>
-                                    <th scope="col">Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {{-- @foreach ($data as $datas)
+                {{-- @include('penggunaan_lahan/modaltambah') --}}
+                <div class="row" style="margin-top: 20px; margin-left: 10px; margin-right: 10px">
+                    <table class="table" id="datatable">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Nama Kecamatan</th>
+                                <th scope="col">Luas Penggunaan Lahan</th>
+                                <th scope="col">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {{-- @foreach ($data as $datas)
                                     <tr>
                                         <th scope="row">{{ $loop->iteration }}</th>
                                         <td>{{ $datas->kecamatan->nama_kecamatan }}</td>
@@ -38,41 +38,41 @@
                                     </tr>
                                 @endforeach
                                 @include('penggunaan_lahan/modaledit') --}}
-                            </tbody>
-                        </table>
-                    </div>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
-    <script>
-        $('.delete').click(function() {
-            var Id = $(this).attr('user-id');
-            alert(Id);
-        });
-    </script>
+</div>
+<script>
+    $('.delete').click(function() {
+        var Id = $(this).attr('user-id');
+        alert(Id);
+    });
+</script>
 @endsection
 @section('footer')
 
-    <script>
-        $('.hapus').click(function() {
-            var Id = $(this).attr('id');
-            var Jenis = $(this).attr('jenis');
-            Swal.fire({
-                    title: 'Yakin?',
-                    text: "Mau Hapus Data Dengan Id " + Id + "?",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Ya',
-                })
-                .then((result) => {
-                    console.log(result);
-                    if (result.value) {
-                        window.location = "/penggunaan_lahan/hapus/" + Id + "";
-                    }
-                });
-        });
-    </script>
+<script>
+    $('.hapus').click(function() {
+        var Id = $(this).attr('id');
+        var Jenis = $(this).attr('jenis');
+        Swal.fire({
+                title: 'Yakin?',
+                text: "Mau Hapus Data Dengan Id " + Id + "?",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Ya',
+            })
+            .then((result) => {
+                console.log(result);
+                if (result.value) {
+                    window.location = "/penggunaan_lahan/hapus/" + Id + "";
+                }
+            });
+    });
+</script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
 
 Route::get('/', function () {
     return view('landing_page.index');
@@ -33,25 +24,25 @@ Route::group(['middleware' => ['auth', 'checkRole:Admin']], function () {
     Route::post('/kelola_kepalakantor/update', 'KelolakepalakantorController@update');
     Route::get('/kelola_kepalakantor/hapus/{id}', 'KelolakepalakantorController@destroy');
     Route::get('/getdatakepalakantor/{id}', 'KelolakepalakantorController@getdatakepalakantor')->name('get.data.pengguna');
-   
+
     Route::get('/kelola_gapoktani', 'GapoktaniController@index');
     Route::post('/kelola_gapoktani/input', 'GapoktaniController@store');
     Route::post('/kelola_gapoktani/update', 'GapoktaniController@update');
     Route::get('/getdatagapoktani/{id}', 'GapoktaniController@getdatagapoktani')->name('get.data.gapoktani');
     Route::get('/kelola_gapoktani/hapus/{id}', 'GapoktaniController@destroy');
-    
+
     Route::get('/kelola_produksi', 'ProduksiController@index');
     Route::post('/kelola_produksi/input', 'ProduksiController@store');
     Route::post('/kelola_produksi/update', 'ProduksiController@update');
     Route::get('/getdataproduksi/{id}', 'ProduksiController@getdataproduksi')->name('get.data.produksi');
     Route::get('/kelola_produksi/hapus/{id}', 'ProduksiController@destroy');
-   
+
     Route::get('/kelola_data_potensi_lahan_pertanian', 'PotensilahanpertanianController@index');
     Route::post('/kelola_data_potensi_lahan_pertanian/input', 'PotensilahanpertanianController@store');
     Route::post('/kelola_data_potensi_lahan_pertanian/update', 'PotensilahanpertanianController@update');
     Route::get('/getdatapotensi/{id}', 'PotensilahanpertanianController@getdatapotensi')->name('get.data.potensi');
     Route::get('/kelola_data_potensi_lahan_pertanian/hapus/{id}', 'PotensilahanpertanianController@destroy');
-    
+
     Route::get('/kelola_data_komoditi_hasil_panen', 'KomoditihasilpanenController@index');
     Route::post('/kelola_data_komoditi_hasil_panen/input', 'KomoditihasilpanenController@store');
     Route::post('/kelola_data_komoditi_hasil_panen/update', 'KomoditihasilpanenController@update');
