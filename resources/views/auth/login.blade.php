dd4e2b3af3c279957a9aca991ee9d5a0ca718fb5
Muhammad Iqbal <muh.iqbal@student.untan.ac.id>
2021-12-29 19:12:42 +0700

    UBAH NAMA LOGIN

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,56 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lahan Sanggau - Login</title>
+    <link href={{ asset('assets/css/bootstrap.min.css') }} rel="stylesheet">
+    <link href={{ asset('assets/css/datepicker3.css') }} rel="stylesheet">
+    <link href={{ asset('assets/css/styles.css') }} rel="stylesheet">
+    <link href={{ asset('assets/images/sanggau.png') }} rel="shortcut icon">
+    <!--[if lt IE 9]>
+ <script src="js/html5shiv.js"></script>
+ <script src="js/respond.min.js"></script>
+ <![endif]-->
+</head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+<body style="background: url({{ asset('assets/images/sawah1.jpg') }}); background-size: 100%;">
+    <div class="row">
+        <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+            <div class="login-panel panel panel-default">
+                <div class="text-center">
+                    <img src={{ asset('assets/images/sanggau.png') }} width="20%" alt="">
+                </div>
+                <div class="panel-heading text-center">SIG LAHAN SANGGAU</div>
+                <div class="panel-body">
+                    <form role="form" method="POST" action="{{ route('login') }}">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <fieldset>
+                            <div class="form-group">
+                                <input class="form-control" id="email" placeholder="E-mail" name="email" type="email"
+                                    autofocus="">
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="form-group">
+                                <input class="form-control" id="password" placeholder="Password" name="password"
+                                    type="password" value="">
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                </label>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </fieldset>
                     </form>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+        </div><!-- /.col-->
+    </div><!-- /.row -->
+
+
+    <script src={{ asset('assets/js/jquery-1.11.1.min.js') }}></script>
+    <script src={{ asset('assets/js/bootstrap.min.js') }}></script>
+</body>
+
+</html>
