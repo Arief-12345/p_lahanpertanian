40a88bdd4adf18ec673636330700e706f98cf085
Muhammad Iqbal <muh.iqbal@student.untan.ac.id>
2021-12-30 13:33:51 +0700

    change

--- app/Penggunaanlahan.php
+++ app/Penggunaanlahan.php
@@ -7,11 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Penggunaanlahan extends Model
 {
     protected $table = 'penggunaan_lahan';
-    protected $fillable = ['kecamatan_id', 'luas_penggunaan_lahan'];
+    protected $fillable = ['kecamatan_id', 'luas_penggunaan_lahan', 'komoditi_id', 'tahun'];
     protected $guarded = [];
 
     public function kecamatan()
     {
         return $this->belongsTo(Kecamatan::class);
+    }
+
+    public function komoditi()
+    {
+        return $this->belongsTo(Komoditihasilpanen::class);
     }
 }
